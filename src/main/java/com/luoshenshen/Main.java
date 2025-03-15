4b6796652fb2d9c02e3c87c3244f60e4951df097
luoshenshen <luoshenshen@buaa.edu.cn>
2025-03-15 22:54:17 +0800

    init s-rpc

---
+++ src/main/java/com/luoshenshen/Main.java
@@ -0,0 +1,11 @@
+package com.luoshenshen;
+
+/**
+ * @author ShenshenLuo
+ * @date 2025/3/15 22:40
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
